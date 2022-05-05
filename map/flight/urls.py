134ed42fce586919926bece260b5d9d8b62ea2c0
Aliz-f <fad127alireza2@gmail.com>
2022-05-05 15:46:23 +0430

    add offline map

--- map/flight/urls.py
+++ map/flight/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('', map, name='map'),
     path('getData/', getData.as_view(), name='getData'),
+    path('offline/', offlineMap, name='offline-map'),
+
 ]
--- map/flight/views.py
+++ map/flight/views.py
@@ -4,6 +4,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, authentication, permissions
 from .serializer import dataFlightSerializer
+from .models import dataFlight
+
+import os
+import folium
 # Create your views here.
 
 class CsrfExemptSessionAuthentication(authentication.SessionAuthentication):
@@ -39,3 +43,57 @@ class getData(APIView):
             return Response(flight, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"details":str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+class generateMap:
+    def __init__(self):
+        self.flight_list=[]
+        self.data=[]
+        self.colors=['black', 'blanchedalmond', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgreen', 'darkgrey', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkslategrey', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dimgrey', 'dodgerblue', 'firebrick', 'forestgreen', 'fuchsia', 'gainsboro', 'gold', 'goldenrod', 'gray', 'green', 'greenyellow', 'grey', 'hotpink', 'indianred', 'indigo', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lime', 'limegreen', 'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mistyrose', 'moccasin', 'navy', 'olive', 'olivedrab', 'orange', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple', 'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'slategrey', 'springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'yellow', 'yellowgreen']
+        self.colorIter=0
+        self.map=None
+        self.prepareData()
+        self.createMap()
+    
+    def prepareData(self):
+        locationsTemp = []
+        data = dataFlight.objects.all()
+        for item in data:
+            if item.validposition ==1 and item.lat > 0.0 and item.lon> 0.0:
+                eachLocation={
+                    'code':item.flight,
+                    'lat':item.lat,
+                    'long':item.lon,
+                }
+                locationsTemp.append(eachLocation)
+        # print(locationsTemp)
+        for loc in locationsTemp:
+            if loc['code'] not in self.flight_list:
+                self.flight_list.append(loc['code'])
+
+        self.data ={}
+        locTemp=[]
+        self.colorIter=0
+        for flight in self.flight_list:
+            for loc in locationsTemp:
+                if loc['code'] == flight:
+                    locTemp.append(list((float(loc['lat']), float(loc['long']))))
+            self.data[flight]={'locations':locTemp, 'color':self.colors[self.colorIter]}
+            locTemp=[]
+            self.colorIter+=1
+            if self.colorIter>=111:
+                self.colorIter=0
+
+
+    def createMap(self):
+        self.map = folium.Map(location=[32.935342,51.549186])
+        folium.Marker([32.743938, 51.867955], popup="<i>Isfahan International Airport</i>", tooltip='More Details').add_to(self.map)
+        for flight in self.flight_list:
+            folium.PolyLine(self.data[flight]['locations'], tooltip=f"<i>{flight}</i>",color=self.data[flight]['color'],weight=8, opacity=15).add_to(self.map)
+            # folium.t
+        path = os.getcwd()
+        self.map.save(f"{path}/flight/templates/flight/map.html")
+
+
+def offlineMap(request):
+    Map = generateMap()
+    return render(request, 'flight/map.html')
